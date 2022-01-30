13fce183f4d97547c86dd01191c2dbbc6c7f571e
Henry Dihardjo <henry.dihardjo@hotmail.com>
2022-01-30 19:21:47 +0700

    default

--- SQL/rating_weather.sql
+++ SQL/rating_weather.sql
@@ -1,16 +1,20 @@
 with t as (
 	select "date", ("min"+"max")/2 as avg_temp, "min", "max",
-		case when precipitation_normal <= 0.5 then '1'
-		 	when precipitation_normal > 0.5 and precipitation_normal <= 2  then '2'
-		 	when precipitation_normal > 2 and precipitation_normal <= 3  then '3'
-		 	when precipitation_normal > 3 then '4'end as rain
+		case when precipitation_normal <= 0.5 then 'light'
+		 	when precipitation_normal > 0.5 and precipitation_normal <= 2  then 'moderate'
+		 	when precipitation_normal > 2 and precipitation_normal <= 3  then 'heavy'
+		 	when precipitation_normal > 3 then 'very heavy'
+		 end as precipitation
 	from dwh_weather dw
 	group by dw."date"
 )
-select count(stars), stars,
-		--min(stars), max(stars),
-		avg("min"+"max")/2 as avg_temp, rain
+select count(review_id) as number_of_reviews, stars, sum(checkin_count) as checkin_count , sum(tip_count) as tip_count ,
+sum(compliment_count) as compliment_count, precipitation, max("min"), min("max"),round(avg("min"+"max")/2) as avg_temp
 from dwh_review dr
-inner join t
+left outer join t
 on dr."date" = t."date"
-group by stars,rain;
+group by stars,precipitation
+
+select avg(stars), "date"
+from dwh_review dr
+group by "date";
